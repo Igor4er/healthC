afd104c19613f134230535b151bceb16ac2de23f
Igor4er <sherednihenko.ihor@gmail.com>
2023-07-28 12:41:37 +0300

    Set-up  theme

--- frontend/src/root.tsx
+++ frontend/src/root.tsx
@@ -24,7 +24,7 @@ export default component$(() => {
         <RouterHead />
         <ServiceWorkerRegister />
       </head>
-      <body lang="en">
+      <body lang="uk" data-theme="healthc">
         <RouterOutlet />
       </body>
     </QwikCityProvider>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -14,7 +14,18 @@ module.exports = {
       dark: {
         ...require("daisyui/src/theming/themes")["[data-theme=dark]"],
         "danger": "#dc3545",
-      }
+      },
+      healthc: { 
+        "primary": "#45a2d8",
+        "secondary": "#9cbef4", 
+        "accent": "#794ed8",
+        "neutral": "#2e222f",
+        "base-100": "#4b2b55",
+        "info": "#4874e5",
+        "success": "#20d586",
+        "warning": "#ef7a06", 
+        "error": "#f07590", 
+        }, 
     },],
   },
 };
