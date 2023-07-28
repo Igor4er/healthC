fc57bab0549f36293b02e187830a27ae34bfa972
writer-mng <volodymyrpetriv2207@gmail.com>
2023-07-28 12:39:36 +0300

    Refactoring

--- back/main.py
+++ back/main.py
@@ -7,8 +7,9 @@ def calculate_imt(height: int, weight: float):
     return {"height": height, "weight": weight}
 
 
-@app.get("/foots/")
-def calculate_foots(age: int, sex: str, activity: int, height: int, weight: float):
+@app.get("/steps/")
+def calculate_steps(age: int, sex: str, activity: int, height: int, weight: float):
+    # Method for calculting optimal counts of steps
     return {"age": age,
     "sex": sex,
     "activity": activity,
