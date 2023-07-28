773bd8feb2afc268ae16ab01242d34e09bff5c3c
Igor4er <sherednihenko.ihor@gmail.com>
2023-07-28 15:16:37 +0300

    added bmi form component

---
+++ frontend/src/components/BMIForm.tsx
@@ -0,0 +1,9 @@
+import { component$ } from "@builder.io/qwik";
+
+export default component$(() => {
+    return (
+        <>
+        <div class="text-2xl">BMIForm component</div>
+        </>
+    )
+});
